304da170157c8dd9f02089b467364e6f7d425d32
welrikyson felix <welrikyson.dev@gmail.com>
2019-04-06 18:52:34 -0400

    configuracao para expor id de despesa

---
+++ control-api/src/main/java/com/control/controlapi/service/RepositoryConfig.java
@@ -0,0 +1,18 @@
+package com.control.controlapi.service;
+
+import com.control.controlapi.model.Despesa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+/**
+ * RepositoryConfig
+ */
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Despesa.class);
+    }
+}
